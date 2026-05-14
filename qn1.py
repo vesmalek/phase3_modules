83070a43c213b86f48738e16abb8d08cdfe1cb7a
Ismail Malik <vesmalick@gmail.com>
2026-05-14 20:20:35 +0300

    Completed qn 01 of Python Modules

---
+++ qn1.py
@@ -0,0 +1,16 @@
+from math import pi, floor, sqrt
+
+#     a) Print the value of pi
+print()
+print(f"Pi = {pi}")
+
+#     b) Round 7.5 down using math.floor
+
+print()
+print(f"Round 7.5 down using math.floor: {floor(7.5)}")
+
+#     c) Find the square root of 144
+
+print()
+print(f"The square root of 144: {sqrt(144)}")
+print()
