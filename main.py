c95abe21cab163858659181182496244ad8bc186
Ismail Malik <vesmalick@gmail.com>
2026-05-14 20:38:42 +0300

    Completed qn 03 of Python Modules

---
+++ main.py
@@ -0,0 +1,10 @@
+from helpers import calculate_discount, greet_user, is_valid_password
+
+print()
+print(calculate_discount(100, 10))
+
+print()
+print(greet_user('Abuu'))
+
+print()
+print(is_valid_password('jhadijhuahdu7848ue@'))
