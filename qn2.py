5de0946917840cfb7b00b9077da7bb210da032b5
Ismail Malik <vesmalick@gmail.com>
2026-05-14 20:24:11 +0300

    Completed qn 02 of Python Modules

---
+++ qn2.py
@@ -0,0 +1,10 @@
+from datetime import datetime as dt
+
+#     Print the current date and time
+current_date = dt.now()
+print()
+print(f"Current Date: {current_date}")
+
+#     Then print just the current year by accessing .year on the result
+print()
+print(f"Current Year: {current_date.year}")
